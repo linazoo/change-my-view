76ddfae6fbae8f2561bdc84db2df6029cf0d99e9
Lina Zoo <linazooper@gmail.com>
2022-03-21 20:48:32 -0500

    building page with comments and posts

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -3,10 +3,36 @@ import { useParams } from "react-router-dom";
 import { fetchPost } from "../../utils/api";
 
 export default function Post() {
+  const [post, setPost] = React.useState({});
+  const [comments, setComments] = React.useState([]);
+
   const { id } = useParams();
   React.useEffect(() => {
-    fetchPost(id);
+    fetchPost(id).then((data) => {
+      const commentsData = data[1].data.children;
+      setComments(commentsData);
+      const postInfo = data[0].data.children[0].data;
+      setPost(postInfo);
+    });
   }, []);
 
-  return <div>Post</div>;
+  return (
+    <div>
+      <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
+      <p dangerouslySetInnerHTML={{ __html: post.selftext }} />
+
+      <ul>
+        {comments.map((comment) => {
+          return (
+            <li key={comment.data.id}>
+              <div>
+                <p dangerouslySetInnerHTML={{ __html: comment.data.body }} />
+                <p dangerouslySetInnerHTML={{ __html: comment.data.author }} />
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -9,12 +9,11 @@ export function fetchMainPosts(type) {
     });
   });
 }
-// URL: https://www.reddit.com/r/changemyview/comments/tiiwju/cmv_the_us_needs_a_500_or_1000_bill/.json
 
 export function fetchPost(id, title) {
   return fetch(`${api}/comments/${id}/${json}`).then((res) => {
     return res.json().then((data) => {
-      console.log(data);
+      return data;
     });
   });
 }
